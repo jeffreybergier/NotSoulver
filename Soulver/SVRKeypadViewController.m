fe46208b36865e5e1654cfc42f58b3d0d5c52cad
jeffreybergier <jeffburg@jeffburg.com>
2024-07-27 11:03:20 +0900

    added awake from nib to all classes

--- Soulver/SVRKeypadViewController.m
+++ Soulver/SVRKeypadViewController.m
@@ -1,6 +1,14 @@
 #import "SVRKeypadViewController.h"
+#import "SVRTapeModel.h"
 
 @implementation SVRKeypadViewController
+
+-(SVRTapeModel *)model; { return model; }
+
+-(void)awakeFromNib;
+{
+  NSLog(@"%@", self);
+}
 
 - (void)append:(NSButton *)sender
 {
