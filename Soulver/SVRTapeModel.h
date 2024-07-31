1e045c0464429a4d303d12d57eae7ef25a409dd9
jeffreybergier <jeffburg@jeffburg.com>
2024-07-31 21:37:30 +0900

    hooked up the UI to the SVRMathNode

--- Soulver/SVRTapeModel.h
+++ Soulver/SVRTapeModel.h
@@ -1,11 +1,20 @@
 #import <AppKit/AppKit.h>
+#import "SVRMathNode.h"
 
 @interface SVRTapeModel: NSObject
 {
-  NSMutableArray */*<SVRTapeModelOperation>*/ operations;
-  NSMutableString * strokeInput;
+  SVRMathNode *_mathNode;
+  NSString *_latestRender;
 }
 
--(void)appendKeyStroke:(NSString *)aStroke;
+// MARK: Properties
+-(SVRMathNode*)mathNode;
+-(void)setMathNode:(SVRMathNode*)aNode;
+-(NSString*)latestRender;
+-(void)setLatestRender:(NSString*)aString;
++(NSString*)renderDidChangeNotificationName;
+
+// MARK: Usage
+-(void)append:(SVRMathNode*)aNode;
 
 @end
