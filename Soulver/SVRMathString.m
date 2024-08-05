ed297aa209a6de4a89caf3909fc5a6b642bceaff
jeffreybergier <jeffburg@jeffburg.com>
2024-08-05 21:40:54 +0900

    added nscoder support to SVRMathString

--- Soulver/SVRMathString.m
+++ Soulver/SVRMathString.m
@@ -9,6 +9,7 @@
 
 @implementation SVRMathString
 
+// MARK: Main Business Logic
 -(void)appendString:(NSString*)aString;
 {
   NSRange scanRange;
@@ -49,11 +50,13 @@
   }
 }
 
+// MARK: Debugging
 -(NSString*)description;
 {
   return [[super description] stringByAppendingString:_string];
 }
 
+// MARK: Init
 -(id)init;
 {
   self = [super init];
@@ -61,6 +64,14 @@
   return self;
 }
 
+-(id)initWithString:(NSString*)aString;
+{
+  self = [super init];
+  _string = [aString mutableCopy];
+  return self;
+}
+
+// MARK: Validation
 +(BOOL)isValidInput:(NSString*)input;
 {
   NSSet *valid = [NSSet setWithObjects:@".", @"/", @"*", @"+", @"-", @"=", @"^", @"(", @")",
@@ -68,10 +79,35 @@
   return [valid member:input] != nil;
 }
 
+// MARK: Dealloc
 - (void)dealloc
 {
   [_string release];
   [super dealloc];
 }
 
+@end
+
+@implementation SVRMathString (Coding)
+-(void)encodeWithCoder:(NSCoder*)coder;
+{
+  [coder encodeObject:_string];
+}
+-(id)initWithCoder:(NSCoder*)coder;
+{
+  id string;
+  self = [super init];
+  string = [coder decodeObject];
+  if ([string isKindOfClass:[NSString class]]) {
+    _string = string;
+    return self;
+  } else {
+    [self release];
+    return nil;
+  }
+}
+-(id)copyWithZone:(NSZone*)zone;
+{
+  return [[SVRMathString alloc] initWithString:_string];
+}
 @end
