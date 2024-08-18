8d449c96a3bdaf5862ca3ecba74cf1354994b9b5
jeffreybergier <jeffburg@jeffburg.com>
2024-08-18 11:06:42 +0900

    [4.2] added unit tests

---
+++ Soulver/SVRMathString+Tests.h
@@ -0,0 +1,15 @@
+//
+//  SVRMathString+Tests.h
+//  SoulverCommand
+//
+//  Created by Jeffrey Bergier on 2024/08/18.
+//
+
+#import "SVRMathString.h"
+
+@interface SVRMathString (Tests)
+
++(void)executeTests;
+-(void)__testAssertEqual:(NSString*)rhs;
+
+@end
---
+++ Soulver/SVRMathString+Tests.m
@@ -0,0 +1,51 @@
+//
+//  SVRMathString+Tests.m
+//  SoulverCommand
+//
+//  Created by Jeffrey Bergier on 2024/08/18.
+//
+
+#import "SVRMathString+Rendering.h"
+#import "SVRMathString+Tests.h"
+
+@implementation SVRMathString (Tests)
+
++(void)executeTests;
+{
+  [[SVRMathString mathStringWithString:@"5*5="]
+                     __testAssertEqual:@"5*5=25\n"];
+  
+  [[SVRMathString mathStringWithString:@"10/2="]
+                     __testAssertEqual:@"10/2=5\n"];
+  
+  [[SVRMathString mathStringWithString:@"(10+2)*5="]
+                     __testAssertEqual:@"(10+2)*5=60\n"];
+  
+  [[SVRMathString mathStringWithString:@"(10+2)+(8-7)*5="]
+                     __testAssertEqual:@"(10+2)+(8-7)*5=17\n"];
+  
+  [[SVRMathString mathStringWithString:@"5="]
+                     __testAssertEqual:@"5=5\n"];
+  
+  [[SVRMathString mathStringWithString:@"+5="]
+                     __testAssertEqual:@"+5=<ERROR>"];
+  
+  [[SVRMathString mathStringWithString:@"5+="]
+                     __testAssertEqual:@"5+=<ERROR>"];
+  
+  [[SVRMathString mathStringWithString:@"(5*((10+3)*10+2)^2)="]
+                     __testAssertEqual:@"(5*((10+3)*10+2)^2)=87120\n"];
+  
+  [[SVRMathString mathStringWithString:@"(5*((10+3)*10+2)^2="]
+                     __testAssertEqual:@"(5*((10+3)*10+2)^2=<ERROR>"];
+}
+
+-(void)__testAssertEqual:(NSString*)rhs;
+{
+  NSString *lhs = [[self render] string];
+  NSString *message = [NSString stringWithFormat:@"SVRMathStringTest: FAIL: %@ != %@", lhs, rhs];
+  NSAssert([lhs isEqualToString:rhs], message);
+  NSLog(@"SVRMathStringTest: PASS: %@", lhs);
+}
+
+@end
