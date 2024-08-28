4e38062159f9215cf4e163d75fb015632a543da3
jeffreybergier <jeffburg@jeffburg.com>
2024-08-28 20:21:51 +0900

    [4.2] moved SVRRange into Rendering

--- Soulver/SVRMathString+Rendering.h
+++ Soulver/SVRMathString+Rendering.h
@@ -7,8 +7,13 @@
 
 #import <AppKit/AppKit.h>
 #import "SVRMathString.h"
-#import "SVRRange.h"
 
+// MARK: Forward Declarations
+@class SVRMathRange;
+@class SVRBoundingRange;
+@class SVRMathLineModel;
+
+// MARK: SVRMathString (Rendering)
 @interface SVRMathString (Rendering)
 
 // MARK: Render
@@ -24,6 +29,37 @@
                                  forOperators:(NSSet*)operators;
 @end
 
+// MARK: Custom Ranges
+@interface SVRBoundingRange: NSObject
+{
+  NSRange _range;
+  NSString *_contents;
+}
+-(NSRange)range;
+-(NSString*)contents;
+-(id)initWithRange:(NSRange)range contents:(NSString*)contents;
++(id)rangeWithRange:(NSRange)range contents:(NSString*)contents;
+-(NSString*)description;
+@end
+
+@interface SVRMathRange: NSObject
+{
+  NSRange _range;
+  NSString *_lhs;
+  NSString *_rhs;
+  NSString *_operator;
+}
+-(NSRange)range;
+-(NSString*)lhs;
+-(NSString*)rhs;
+-(NSString*)operator;
+-(id)initWithRange:(NSRange)range lhs:(NSString*)lhs rhs:(NSString*)rhs operator:(NSString*)operator;
++(id)rangeWithRange:(NSRange)range lhs:(NSString*)lhs rhs:(NSString*)rhs operator:(NSString*)operator;
+-(NSDecimalNumber*)evaluate;
+-(NSString*)description;
+@end
+
+// MARK: Convert String to Array
 @interface SVRMathLineModel: NSObject
 {
   NSArray *_completeLines;
@@ -36,4 +72,16 @@
 -(id)initWithEncodedString:(NSString*)input;
 +(id)modelWithEncodedString:(NSString*)input;
 -(void)__initProperties:(NSString*)input;
+@end
+
+// MARK: NSString
+
+// MARK: NSString Custom Range Search
+@interface NSString (Searching)
+-(SVRBoundingRange*)SVR_searchRangeBoundedByLHS:(NSString*)lhs
+                                            rhs:(NSString*)rhs
+                                          error:(NSNumber**)error;
+-(SVRMathRange*)SVR_searchMathRangeForOperators:(NSSet*)including
+                           allPossibleOperators:(NSSet*)ignoring
+                            allPossibleNumerals:(NSSet*)numerals;
 @end
