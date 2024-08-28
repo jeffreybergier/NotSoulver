42775a987a5c36c6236d1a15e90e8cf679dedb7b
jeffreybergier <jeffburg@jeffburg.com>
2024-08-28 20:32:20 +0900

    [4.2] moved NSString+Soulver into Rendering

--- Soulver/SVRMathString+Rendering.h
+++ Soulver/SVRMathString+Rendering.h
@@ -10,8 +10,6 @@
 
 // MARK: Forward Declarations
 @class SVRMathRange;
-@class SVRBoundingRange;
-@class SVRMathLineModel;
 
 // MARK: SVRMathString (Rendering)
 @interface SVRMathString (Rendering)
@@ -76,12 +74,60 @@
 
 // MARK: NSString
 
-// MARK: NSString Custom Range Search
-@interface NSString (Searching)
+@interface NSString (Soulver)
 -(SVRBoundingRange*)SVR_searchRangeBoundedByLHS:(NSString*)lhs
                                             rhs:(NSString*)rhs
                                           error:(NSNumber**)error;
 -(SVRMathRange*)SVR_searchMathRangeForOperators:(NSSet*)including
                            allPossibleOperators:(NSSet*)ignoring
                             allPossibleNumerals:(NSSet*)numerals;
+-(BOOL)SVR_containsOnlyCharactersInSet:(NSSet*)set;
+-(BOOL)SVR_beginsWithCharacterInSet:(NSSet*)set;
+-(BOOL)SVR_endsWithCharacterInSet:(NSSet*)set;
+-(NSString*)SVR_stringByMappingCharactersInDictionary:(NSDictionary*)map;
+@end
+
+// MARK: NSMutableString
+@interface NSMutableString (Soulver)
+-(void)SVR_insertSolution:(id)solution
+                  atRange:(NSRange)range
+                    error:(NSNumber**)error;
+-(BOOL)__canInsertSolutionAtRange:(NSRange)range;
+@end
+
+// MARK: NSAttributedString
+@interface NSAttributedString (Soulver)
++(id)SVR_stringWithString:(NSString*)aString;
++(id)SVR_stringWithString:(NSString*)aString color:(NSColor*)aColor;
+@end
+
+// MARK: NSStringEnumerator
+@interface SVRStringEnumeratorRange: NSObject
+{
+  NSRange _range;
+  NSString *_substring;
+}
+-(NSRange)range;
+-(NSString*)substring;
+-(NSString*)description;
+-(id)initWithRange:(NSRange)range substring:(NSString*)substring;
++(id)rangeWithRange:(NSRange)range substring:(NSString*)substring;
+@end
+
+@interface SVRStringEnumerator: NSEnumerator
+{
+  NSString *_string;
+  NSRange _range;
+}
+-(SVRStringEnumeratorRange*)nextObject;
+-(id)initWithString:(NSString*)string;
++(id)enumeratorWithString:(NSString*)string;
+@end
+
+// MARK: NSDecimalNumber
+@interface NSDecimalNumber (Soulver)
+/// In OpenStep, NaN comparisons are weird, so this uses a string comparison
+-(BOOL)SVR_isNotANumber;
+-(NSString*)SVR_description;
++(id)SVR_decimalNumberWithString:(NSString*)string;
 @end
--- Soulver/SVRMathString+Rendering.m
+++ Soulver/SVRMathString+Rendering.m
@@ -6,7 +6,6 @@
 //
 
 #import "SVRMathString+Rendering.h"
-#import "NSString+Soulver.h"
 #import "SVRConstants.h"
 
 // MARK: SVRMathString (Rendering)
@@ -174,6 +173,7 @@
             ]
   ];
 }
+
 -(NSAttributedString*)render_colorSolution:(NSString*)solution;
 {
   return [NSAttributedString SVR_stringWithString:solution color:[NSColor cyanColor]];
@@ -191,14 +191,17 @@
 
 // MARK: Custom Ranges
 @implementation SVRBoundingRange
+
 -(NSRange)range;
 {
   return _range;
 }
+
 -(NSString*)contents;
 {
   return _contents;
 }
+
 -(id)initWithRange:(NSRange)range contents:(NSString*)contents;
 {
   self = [super init];
@@ -206,16 +209,19 @@
   _contents = [contents retain];
   return self;
 }
+
 +(id)rangeWithRange:(NSRange)range contents:(NSString*)contents;
 {
   return [[[SVRBoundingRange alloc] initWithRange:range
                                          contents:contents] autorelease];
 }
+
 -(NSString*)description;
 {
   return [NSString stringWithFormat:@"SVRBoundingRange: \"%@\" {%lu, %lu}",
                                     _contents, _range.location, _range.length];
 }
+
 - (void)dealloc
 {
   [_contents release];
@@ -224,22 +230,27 @@
 @end
 
 @implementation SVRMathRange
+
 -(NSRange)range;
 {
   return _range;
 }
+
 -(NSString*)lhs;
 {
   return _lhs;
 }
+
 -(NSString*)rhs;
 {
   return _rhs;
 }
+
 -(NSString*)operator;
 {
   return _operator;
 }
+
 -(id)initWithRange:(NSRange)range lhs:(NSString*)lhs rhs:(NSString*)rhs operator:(NSString*)operator;
 {
   self = [super init];
@@ -249,6 +260,7 @@
   _operator = [operator retain];
   return self;
 }
+
 +(id)rangeWithRange:(NSRange)range lhs:(NSString*)lhs rhs:(NSString*)rhs operator:(NSString*)operator;
 {
   return [[[SVRMathRange alloc] initWithRange:range
@@ -256,6 +268,7 @@
                                           rhs:rhs
                                      operator:operator] autorelease];
 }
+
 -(NSDecimalNumber*)evaluate;
 {
   NSDecimalNumber *lhs = [NSDecimalNumber SVR_decimalNumberWithString:[self lhs]];
@@ -282,11 +295,13 @@
     return nil;
   }
 }
+
 -(NSString*)description;
 {
   return [NSString stringWithFormat:@"SVRMathRange: <%@><%@><%@> {%lu, %lu}",
                                     _lhs, _operator, _rhs, _range.location, _range.length];
 }
+
 - (void)dealloc
 {
   [_lhs release];
@@ -294,9 +309,11 @@
   [_operator release];
   [super dealloc];
 }
+
 @end
 
 @implementation SVRMathLineModel
+
 -(NSArray*)completeLines;
 {
   return _completeLines;
@@ -312,6 +329,7 @@
   [self __initProperties:input];
   return self;
 }
+
 +(id)modelWithEncodedString:(NSString*)input;
 {
   return [[[SVRMathLineModel alloc] initWithEncodedString:input] autorelease];
@@ -350,10 +368,11 @@
   _incompleteLine = nil;
   [super dealloc];
 }
+
 @end
 
-// MARK: NSString Custom Range Search
-@implementation NSString (Searching)
+// MARK: NSString
+@implementation NSString (Soulver)
 
 -(SVRBoundingRange*)SVR_searchRangeBoundedByLHS:(NSString*)lhs
                                             rhs:(NSString*)rhs
@@ -452,4 +471,254 @@
     return nil;
   }
 }
+
+-(BOOL)SVR_containsOnlyCharactersInSet:(NSSet*)set;
+{
+  SVRStringEnumeratorRange *next;
+  NSEnumerator *e = [SVRStringEnumerator enumeratorWithString:self];
+  
+  while ((next = [e nextObject])) {
+    if ([set member:[next substring]]) { continue; }
+    return NO;
+  }
+  
+  return YES;
+}
+
+-(BOOL)SVR_beginsWithCharacterInSet:(NSSet*)set;
+{
+  if ([self length] == 0) { return NO; }
+  return [set member:[self substringWithRange:NSMakeRange(0, 1)]] != nil;
+}
+
+-(BOOL)SVR_endsWithCharacterInSet:(NSSet*)set;
+{
+  if ([self length] == 0) { return NO; }
+  return [set member:[self substringWithRange:NSMakeRange([self length] - 1, 1)]] != nil;
+}
+
+-(NSString*)SVR_stringByMappingCharactersInDictionary:(NSDictionary*)map;
+{
+  SVRStringEnumeratorRange *next = nil;
+  NSEnumerator *e = [SVRStringEnumerator enumeratorWithString:self];
+  NSMutableString *output = [[NSMutableString new] autorelease];
+  NSString *toAppend = nil;
+  while ((next = [e nextObject])) {
+    toAppend = [map objectForKey:[next substring]];
+    if (!toAppend) { toAppend = [next substring]; }
+    [output appendString:toAppend];
+  }
+  return [[output copy] autorelease];
+}
+
+@end
+
+// MARK: NSMutableString
+@implementation NSMutableString (Soulver)
+
+-(void)SVR_insertSolution:(id)solution
+                  atRange:(NSRange)range
+                    error:(NSNumber**)error;
+{
+  NSString *solutionString;
+  BOOL problem = NO;
+  
+  if (*error != nil) { return; }
+  if (![self __canInsertSolutionAtRange:range]) { *error = [NSNumber SVR_errorPatching]; return; }
+
+  if ([solution isKindOfClass:[NSDecimalNumber class]]) {
+    problem = [solution SVR_isNotANumber];
+    solutionString = [solution SVR_description];
+  } else if ([solution isKindOfClass:[NSString class]]) {
+    problem = [[NSDecimalNumber SVR_decimalNumberWithString:solution] SVR_isNotANumber];
+    solutionString = solution;
+  } else {
+    [NSException raise:@"UnexpectedTypeException" format:@"Expected NSDecimalNumber or NSString: Got %@", solution];
+    solutionString = nil;
+  }
+  
+  if (problem) {
+    *error = [NSNumber SVR_errorInvalidCharacter];
+    return;
+  }
+  
+  [self replaceCharactersInRange:range withString:solutionString];
+  return;
+}
+
+-(BOOL)__canInsertSolutionAtRange:(NSRange)range;
+{
+  BOOL issueFound = NO;
+  BOOL canCheckLeft = NO;
+  BOOL canCheckRight = NO;
+  NSRange checkRange = NSMakeRange(0,0);
+  
+  canCheckLeft = range.location > 0;
+  canCheckRight = range.location + range.length < [self length];
+  
+  // Perform Checks to the left and right to make sure
+  // there are operators outside of where we are patching.
+  if (canCheckLeft) {
+    checkRange.location = range.location - 1;
+    checkRange.length = 1;
+    issueFound = [[NSSet SVR_solutionInsertCheck] member:[self substringWithRange:checkRange]] == nil;
+  }
+  
+  if (issueFound) {
+    return NO;
+  }
+  
+  if (canCheckRight) {
+    checkRange.location = range.location + range.length;
+    checkRange.length = 1;
+    issueFound = [[NSSet SVR_solutionInsertCheck] member:[self substringWithRange:checkRange]] == nil;
+  }
+  
+  if (issueFound) {
+    return NO;
+  }
+  
+  return YES;
+}
+
+@end
+
+// MARK: NSAttributedString
+@implementation NSAttributedString (Soulver)
+
++(id)SVR_stringWithString:(NSString*)aString;
+{
+  return [self SVR_stringWithString:aString color:nil];
+}
+
++(id)SVR_stringWithString:(NSString*)aString color:(NSColor*)aColor;
+{
+  NSArray      *keys;
+  NSArray      *vals;
+  NSFont       *font;
+  NSDictionary *attr;
+  
+  font = [NSFont userFixedPitchFontOfSize:14];
+  
+  if (aColor) {
+    keys = [NSArray arrayWithObjects:NSBackgroundColorAttributeName, NSFontAttributeName, nil];
+    vals = [NSArray arrayWithObjects:aColor, font, nil];
+    attr = [NSDictionary dictionaryWithObjects:vals forKeys:keys];
+    return [[[NSAttributedString alloc] initWithString:aString attributes:attr] autorelease];
+  } else {
+    keys = [NSArray arrayWithObjects:NSFontAttributeName, nil];
+    vals = [NSArray arrayWithObjects:font, nil];
+    attr = [NSDictionary dictionaryWithObjects:vals forKeys:keys];
+    return [[[NSAttributedString alloc] initWithString:aString attributes:attr] autorelease];
+  }
+}
+@end
+
+@implementation SVRStringEnumeratorRange
+
+-(NSRange)range;
+{
+  return _range;
+}
+
+-(NSString*)substring;
+{
+  return _substring;
+}
+
+-(NSString*)description;
+{
+  return [
+    NSString stringWithFormat:@"SVREnumeratorRange: '%@' {%lu, %lu}",
+                              _substring, _range.location, _range.length];
+}
+
+-(id)initWithRange:(NSRange)range substring:(NSString*)substring;
+{
+  self = [super init];
+  _range = range;
+  _substring = [substring retain];
+  return self;
+}
+
++(id)rangeWithRange:(NSRange)range substring:(NSString*)substring;
+{
+  return [[[SVRStringEnumeratorRange alloc] initWithRange:range substring:substring] autorelease];
+}
+
+- (void)dealloc
+{
+  [_substring release];
+  [super dealloc];
+}
+
+@end
+
+@implementation SVRStringEnumerator
+
+-(SVRStringEnumeratorRange*)nextObject;
+{
+  SVRStringEnumeratorRange *output;
+  NSString *substring;
+  
+  if ([_string length] == 0) {
+    return nil;
+  }
+  
+  if (_range.location > [_string length] - _range.length) {
+    return nil;
+  }
+  
+  substring = [_string substringWithRange:_range];
+  output = [SVRStringEnumeratorRange rangeWithRange:_range substring:substring];
+  
+  _range.location += 1;
+  return output;
+}
+
+-(id)initWithString:(NSString*)string;
+{
+  self = [super init];
+  _string = [string retain];
+  _range = NSMakeRange(0,1);
+  return self;
+}
+
++(id)enumeratorWithString:(NSString*)string;
+{
+  return [[[SVRStringEnumerator alloc] initWithString:string] autorelease];
+}
+
+- (void)dealloc
+{
+  [_string release];
+  [super dealloc];
+}
+
+@end
+
+// MARK: NSDecimalNumber
+@implementation NSDecimalNumber (Soulver)
+
+-(BOOL)SVR_isNotANumber;
+{
+  NSString *lhsDescription;
+  NSString *rhsDescription;
+  
+  lhsDescription = [self SVR_description];
+  rhsDescription = [[NSDecimalNumber notANumber] SVR_description];
+  
+  return [lhsDescription isEqualToString:rhsDescription];
+}
+
+-(NSString*)SVR_description;
+{
+  return [self descriptionWithLocale:[NSDictionary SVR_numberLocale]];
+}
+
++(id)SVR_decimalNumberWithString:(NSString*)string;
+{
+  return [NSDecimalNumber decimalNumberWithString:string locale:[NSDictionary SVR_numberLocale]];
+}
+
 @end
