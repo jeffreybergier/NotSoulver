2635d19de8a2a5d67bff02c08016e5c488c0c351
jeffreybergier <jeffburg@jeffburg.com>
2024-09-06 13:17:49 +0900

    [4.2] Added basic logging utility

--- Soulver/SVRCrossPlatform.h
+++ Soulver/SVRCrossPlatform.h
@@ -2,3 +2,15 @@
 
 #import <AppKit/AppKit.h>
 
+@interface XPLog: NSObject
+/// Always does an NSLog
++(void)always:(NSString*)formatString, ...;
+/// NSLog when in DEBUG
++(void)debug:(NSString*)formatString, ...;
+/// NSLog when in DEBUG and also pauses debugger
+/// Requires `fb [XPLog pause]` in GDB
++(void)pause:(NSString*)formatString, ...;
+/// NSLog only when DEBUG and EXCESS flag found
+/// Requires `-DEXCESS` CFLAG option in GCC
++(void)excess:(NSString*)formatString, ...;
+@end
--- Soulver/SVRCrossPlatform.m
+++ Soulver/SVRCrossPlatform.m
@@ -2,4 +2,43 @@
 
 #import "SVRCrossPlatform.h"
 
+@implementation XPLog
 
++(void)always:(NSString*)formatString, ...;
+{
+  va_list args;
+  va_start(args, formatString);
+  NSLog(formatString, args);
+  va_end(args);
+}
+
++(void)debug:(NSString*)formatString, ...;
+{
+#if DEBUG
+  va_list args;
+  va_start(args, formatString);
+  NSLog(formatString, args);
+  va_end(args);
+#endif
+}
+
++(void)pause:(NSString*)formatString, ...;
+{
+#if DEBUG
+  va_list args;
+  va_start(args, formatString);
+  NSLog(formatString, args);
+  va_end(args);
+#endif
+}
+
++(void)excess:(NSString*)formatString, ...;
+{
+#if DEBUG && EXCESS
+  va_list args;
+  va_start(args, formatString);
+  NSLog(formatString, args);
+  va_end(args);
+#endif
+}
+@end
