b26bf9f8b1001cb034d7b184540cd604ab7689e2
jeffreybergier <jeffburg@jeffburg.com>
2024-10-25 08:39:31 +0900

    [4.2] updated SLRERegex to support capture groups

--- Soulver/SLRERegex.h
+++ Soulver/SLRERegex.h
@@ -9,28 +9,37 @@
 #import "XPCrossPlatform.h"
 #import "slre.h"
 
+@class SLRERegexMatch;
+
 @interface SLRERegex: NSEnumerator
 {
   mm_copy NSString *_pattern;
   mm_copy NSString *_string;
   
+  int _groupCount;
   int _bufferIndex;
   int _bufferLength;
   struct slre _engine;
 }
 
 // MARK: Initialization
--(id)initWithString:(NSString*)string pattern:(NSString*)pattern;
-+(id)regexWithString:(NSString*)string pattern:(NSString*)pattern;
+-(id)initWithString:(NSString*)string
+            pattern:(NSString*)pattern
+         groupCount:(int)groupCount;
+-(id)initWithString:(NSString*)string
+            pattern:(NSString*)pattern;
++(id)regexWithString:(NSString*)string
+             pattern:(NSString*)pattern
+          groupCount:(int)groupCount;
++(id)regexWithString:(NSString*)string
+             pattern:(NSString*)pattern;
 
 // MARK: Core Functionality
-/// Returns NSNotFound for location when there are no more matches
 -(BOOL)containsMatch;
--(NSRange)nextMatch;
 
 // MARK: NSEnumerator
--(NSArray*)allObjects;
--(NSValue*)nextObject;
+// TODO: Change this type to SLRERegexMatch
+-(id)nextObject;
 
 // MARK: Convenience Properties
 -(NSString*)string;
@@ -39,8 +48,27 @@
 
 @end
 
+@interface SLRERegexMatch: NSObject
+{
+  mm_retain NSArray *_groupRanges;
+  NSRange _range;
+}
+
+// MARK: Properties
+-(NSRange)range;
+-(NSArray*)groupRanges;
+
+// MARK: Init
+-(id)initWithRange:(NSRange)matchRange
+       groupRanges:(NSArray*)groupRanges;
++(id)matchWithRange:(NSRange)matchRange
+        groupRanges:(NSArray*)groupRanges;
+
+// MARK: Convenient Methods
+-(NSRange)groupRangeAtIndex:(XPUInteger)index;
+
+@end
+
 @interface SLRERegex (Tests)
 +(void)executeTests;
-+(void)__executeTests_ranges;
-+(void)__executeTests_values;
 @end
--- Soulver/SLRERegex.m
+++ Soulver/SLRERegex.m
@@ -11,12 +11,15 @@
 
 // MARK: Initialization
 
--(id)initWithString:(NSString*)string pattern:(NSString*)pattern;
+-(id)initWithString:(NSString*)string
+            pattern:(NSString*)pattern
+         groupCount:(int)groupCount;
 {
   BOOL isCompiled = NO;
   self = [super init];
   _pattern = [pattern copy];
   _string = [string copy];
+  _groupCount = groupCount + 1; // according to documentation in slre.h
   _bufferIndex = 0;
   _bufferLength = (int)[string length];
   isCompiled = slre_compile(&_engine, [_pattern XP_UTF8String]);
@@ -24,9 +27,28 @@
   return self;
 }
 
+-(id)initWithString:(NSString*)string
+            pattern:(NSString*)pattern;
+{
+  return [self initWithString:string
+                      pattern:pattern
+                   groupCount:0];
+}
+
++(id)regexWithString:(NSString*)string
+             pattern:(NSString*)pattern
+          groupCount:(int)groupCount;
+{
+  return [[[SLRERegex alloc] initWithString:string
+                                    pattern:pattern
+                                 groupCount:groupCount] autorelease];
+}
+
 +(id)regexWithString:(NSString*)string pattern:(NSString*)pattern;
 {
-  return [[[SLRERegex alloc] initWithString:string pattern:pattern] autorelease];
+  return [self regexWithString:string
+                       pattern:pattern
+                    groupCount:0];
 }
 
 // MARK: Core Functionality
@@ -34,49 +56,48 @@
 {
   BOOL containsMatch = NO;
   const char* buffer = [_string XP_UTF8String];
+  struct cap captures[_groupCount];
   containsMatch = slre_match(&_engine,
                              buffer + _bufferIndex,
                              _bufferLength - _bufferIndex,
-                             NULL);
+                             captures);
   return containsMatch;
 }
 
--(NSRange)nextMatch;
+-(SLRERegexMatch*)nextObject;
 {
-  NSRange output = XPNotFoundRange;
+  NSRange matchRange = XPNotFoundRange;
+  NSRange groupRange = XPNotFoundRange;
+  NSMutableArray *groupRanges = [[NSMutableArray new] autorelease];
   BOOL containsMatch = NO;
   const char* buffer = [_string XP_UTF8String];
-  struct cap capture;
+  struct cap captures[_groupCount];
+  int idx;
   
   containsMatch = slre_match(&_engine,
                              buffer + _bufferIndex,
                              _bufferLength - _bufferIndex,
-                             &capture);
+                             captures);
   
-  if (!containsMatch) { return output; }
+  if (!containsMatch) { return nil; }
   
-  output.location = (XPUInteger)(capture.ptr - buffer);
-  output.length = (XPUInteger)capture.len;
-  _bufferIndex = (int)NSMaxRange(output);
-    
-  return output;
-}
-
-// MARK: NSEnumerator
--(NSArray*)allObjects;
-{
-  NSLog(@"JSBRegex does not implement -allObjects");
-  return nil;
-}
-
--(NSValue*)nextObject;
-{
-  NSRange range = [self nextMatch];
-  if (XPIsNotFoundRange(range)) {
-    return nil;
-  } else {
-    return [NSValue valueWithBytes:&range objCType:@encode(NSRange)];
+  for (idx = 0; idx<_groupCount; idx++) {
+    // pull out the full range and update the bufferIndex for next iteration
+    if (idx == 0) {
+      matchRange.location = (XPUInteger)(captures[idx].ptr - buffer);
+      matchRange.length = (XPUInteger)captures[idx].len;
+      NSLog(@"%@",[_string SVR_descriptionHighlightingRange:matchRange]);
+      _bufferIndex = (int)NSMaxRange(matchRange);
+    } else {
+      groupRange.location = (XPUInteger)(captures[idx].ptr - buffer);
+      groupRange.length = (XPUInteger)captures[idx].len;
+      NSLog(@"%@",[_string SVR_descriptionHighlightingRange:groupRange]);
+      [groupRanges addObject:[NSValue XP_valueWithRange:groupRange]];
+    }
   }
+  
+  return [SLRERegexMatch matchWithRange:matchRange
+                            groupRanges:[[groupRanges copy] autorelease]];
 }
 
 // MARK: Convenience Properties
@@ -109,21 +130,95 @@
 
 @end
 
+@implementation SLRERegexMatch
+
+// MARK: Properties
+
+-(NSRange)range;
+{
+  return _range;
+}
+-(NSArray*)groupRanges;
+{
+  return [[_groupRanges retain] autorelease];
+}
+
+// MARK: Init
+
+-(id)initWithRange:(NSRange)matchRange
+       groupRanges:(NSArray*)groupRanges;
+{
+  self = [super init];
+  _range = matchRange;
+  _groupRanges = [groupRanges retain];
+  return self;
+}
+
++(id)matchWithRange:(NSRange)matchRange
+        groupRanges:(NSArray*)groupRanges;
+{
+  return [[[SLRERegexMatch alloc] initWithRange:matchRange
+                                    groupRanges:groupRanges] autorelease];
+}
+
+// MARK: Convenient Methods
+-(NSRange)groupRangeAtIndex:(XPUInteger)index;
+{
+  return [[[self groupRanges] objectAtIndex:index] XP_rangeValue];
+}
+
+- (void)dealloc
+{
+  [XPLog extra:@"DEALLOC: %@", self];
+  [_groupRanges release];
+  _groupRanges = nil;
+  [super dealloc];
+}
+
+@end
+
 @implementation SLRERegex (Tests)
 
 +(void)executeTests;
 {
+  SLRERegex *regex = nil;
+  SLRERegexMatch *match = nil;
+  
   NSLog(@"%@ Unit Tests: STARTING", self);
-  [self __executeTests_ranges];
-  [self __executeTests_values];
+  
+  // MARK: Test Capture Groups
+  regex = [SLRERegex regexWithString:@"and 5+5 and 4-4 and 6*6 and 7/6 and 8^8 and"
+                             pattern:@"\\d(\\+|\\-|\\/|\\*|\\^)\\d"
+                          groupCount:1];
+  NSAssert([regex containsMatch], @"");
+  match = [regex nextObject];
+  NSAssert([[match groupRanges] count] == 1, @"");
+  NSAssert([[[regex string] substringWithRange:[match range]] isEqualToString:@"5+5"], @"");
+  NSAssert([[[regex string] substringWithRange:[match groupRangeAtIndex:0]] isEqualToString:@"+"], @"");
+  match = [regex nextObject];
+  NSAssert([[match groupRanges] count] == 1, @"");
+  NSAssert([[[regex string] substringWithRange:[match range]] isEqualToString:@"4-4"], @"");
+  NSAssert([[[regex string] substringWithRange:[match groupRangeAtIndex:0]] isEqualToString:@"-"], @"");
+  match = [regex nextObject];
+  NSAssert([[match groupRanges] count] == 1, @"");
+  NSAssert([[[regex string] substringWithRange:[match range]] isEqualToString:@"6*6"], @"");
+  NSAssert([[[regex string] substringWithRange:[match groupRangeAtIndex:0]] isEqualToString:@"*"], @"");
+  match = [regex nextObject];
+  NSAssert([[match groupRanges] count] == 1, @"");
+  NSAssert([[[regex string] substringWithRange:[match range]] isEqualToString:@"7/6"], @"");
+  NSAssert([[[regex string] substringWithRange:[match groupRangeAtIndex:0]] isEqualToString:@"/"], @"");
+  match = [regex nextObject];
+  NSAssert([[match groupRanges] count] == 1, @"");
+  NSAssert([[[regex string] substringWithRange:[match range]] isEqualToString:@"8^8"], @"");
+  NSAssert([[[regex string] substringWithRange:[match groupRangeAtIndex:0]] isEqualToString:@"^"], @"");
+  match = [regex nextObject];
+  NSAssert(match == nil, @"");
+  
+  
   NSLog(@"%@ Unit Tests: PASSED", self);
 }
-
-+(void)__executeTests_ranges;
-{
-  NSRange range;
-  SLRERegex *regex;
   
+  /*
   // MARK: Test basic string matching
   regex = [SLRERegex regexWithString:@"this is a verb for other cool verbs" pattern:@"verb"];
   NSAssert(regex, @"");
@@ -212,94 +307,5 @@
   NSAssert(range.length == 1, @"");
 }
 
-+(void)__executeTests_values;
-{
-  NSRange range = XPNotFoundRange;
-  NSValue *value = nil;
-  SLRERegex *regex = nil;
-  
-  // MARK: Test basic string matching
-  regex = [SLRERegex regexWithString:@"this is a verb for other cool verbs" pattern:@"verb"];
-  NSAssert(regex, @"");
-  NSAssert([regex containsMatch], @"");
-  value = [regex nextObject];
-  [value getValue:&range];
-  NSAssert(range.location == 10, @"");
-  NSAssert(range.length == 4, @"");
-  NSAssert([[[regex string] substringWithRange:range] isEqualToString:@"verb"], @"");
-  value = [regex nextObject];
-  [value getValue:&range];
-  NSAssert(range.location == 30, @"");
-  NSAssert(range.length == 4, @"");
-  NSAssert([[[regex string] substringWithRange:range] isEqualToString:@"verb"], @"");
-  
-  // MARK: Test simple numbers
-  regex = [SLRERegex regexWithString:@"12.34a5678m-90.12s-3456" pattern:@"-?\\d+\\.?\\d+"];
-  NSAssert([regex containsMatch], @"");
-  value = [regex nextObject];
-  [value getValue:&range];
-  NSAssert(range.location == 0, @"");
-  NSAssert(range.length == 5, @"");
-  NSAssert([[[regex string] substringWithRange:range] isEqualToString:@"12.34"], @"");
-  value = [regex nextObject];
-  [value getValue:&range];
-  NSAssert(range.location == 6, @"");
-  NSAssert(range.length == 4, @"");
-  NSAssert([[[regex string] substringWithRange:range] isEqualToString:@"5678"], @"");
-  value = [regex nextObject];
-  [value getValue:&range];
-  NSAssert(range.location == 11, @"");
-  NSAssert(range.length == 6, @"");
-  NSAssert([[[regex string] substringWithRange:range] isEqualToString:@"-90.12"], @"");
-  value = [regex nextObject];
-  [value getValue:&range];
-  NSAssert(range.location == 18, @"");
-  NSAssert(range.length == 5, @"");
-  NSAssert([[[regex string] substringWithRange:range] isEqualToString:@"-3456"], @"");
-  value = [regex nextObject];
-  NSAssert(!value, @"");
-  
-  // MARK: Test expression finding
-  regex = [SLRERegex regexWithString:@"12.34a5678m999m-90.12s-3456" pattern:@"-?\\d+\\.?\\d+[ma]-?\\d+\\.?\\d+"];
-  NSAssert([regex containsMatch], @"");
-  value = [regex nextObject];
-  [value getValue:&range];
-  NSAssert(range.location == 0, @"");
-  NSAssert(range.length == 10, @"");
-  NSAssert([[[regex string] substringWithRange:range] isEqualToString:@"12.34a5678"], @"");
-  value = [regex nextObject];
-  [value getValue:&range];
-  NSAssert(range.location == 11, @"");
-  NSAssert(range.length == 10, @"");
-  NSAssert([[[regex string] substringWithRange:range] isEqualToString:@"999m-90.12"], @"");
-  value = [regex nextObject];
-  NSAssert(!value, @"");
-  
-  // MARK: Test expressions with bad numbers
-  regex = [SLRERegex regexWithString:@"12.m56...78m--90.12" pattern:@"-?\\d+\\.?\\d+m-?\\d+\\.?\\d+"];
-  NSAssert(![regex containsMatch], @"");
-  
-  // MARK: Test finding exponent
-  regex = [SLRERegex regexWithString:@"3*5^2+7" pattern:@"\\d\\^\\d"];
-  NSAssert([regex containsMatch], @"");
-  value = [regex nextObject];
-  [value getValue:&range];
-  NSAssert(range.location == 2, @"");
-  NSAssert(range.length == 3, @"");
-  regex = [SLRERegex regexWithString:@"3*5/2+7" pattern:@"\\d\\^\\d"];
-  NSAssert(![regex containsMatch], @"");
-  value = [regex nextObject];
-  NSAssert(!value, @"");
-  regex = [SLRERegex regexWithString:@"3*5^2" pattern:@"[\\^\\*]"];
-  NSAssert([regex containsMatch], @"");
-  value = [regex nextObject];
-  [value getValue:&range];
-  NSAssert(range.location == 1, @"");
-  NSAssert(range.length == 1, @"");
-  value = [regex nextObject];
-  [value getValue:&range];
-  NSAssert(range.location == 3, @"");
-  NSAssert(range.length == 1, @"");
-}
-
+*/
 @end
