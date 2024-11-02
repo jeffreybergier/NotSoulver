ddbb477e8c23b6e83782c3fc6278d1d80ded1f43
jeffreybergier <jeffburg@jeffburg.com>
2024-11-02 22:42:39 +0900

    [4.2] more progress on settings screen

--- Soulver/SVRSettingsViewController.h
+++ Soulver/SVRSettingsViewController.h
@@ -1,35 +1,49 @@
 #import <AppKit/AppKit.h>
+#import "NSUserDefaults+Soulver.h"
 #import "XPCrossPlatform.h"
 
 @interface SVRSettingsViewController: NSResponder
 {
-  mm_unretain IBOutlet NSWindow  *_window;
-  mm_retain   IBOutlet NSView    *_groupGeneralView;
-  mm_retain   IBOutlet NSView    *_groupColorView;
-  mm_retain   IBOutlet NSView    *_groupFontView;
-  mm_unretain IBOutlet NSControl *_settingsChooser;
-  mm_unretain IBOutlet NSControl *_wellDarkBackground;
-  mm_unretain IBOutlet NSControl *_wellDarkInsertionPoint;
-  mm_unretain IBOutlet NSControl *_wellDarkOperand;
-  mm_unretain IBOutlet NSControl *_wellDarkOperator;
-  mm_unretain IBOutlet NSControl *_wellDarkOther;
-  mm_unretain IBOutlet NSControl *_wellDarkParenthesis;
-  mm_unretain IBOutlet NSControl *_wellDarkSolution;
-  mm_unretain IBOutlet NSControl *_wellLightBackground;
-  mm_unretain IBOutlet NSControl *_wellLightError;
-  mm_unretain IBOutlet NSControl *_wellLightInsertionPoint;
-  mm_unretain IBOutlet NSControl *_wellLightOperand;
-  mm_unretain IBOutlet NSControl *_wellLightOperator;
-  mm_unretain IBOutlet NSControl *_wellLightOther;
-  mm_unretain IBOutlet NSControl *_wellLightParenthesis;
-  mm_unretain IBOutlet NSControl *_wellLightSolution;
+  mm_unretain IBOutlet NSWindow *_window;
+  mm_retain   IBOutlet NSView   *_groupGeneralView;
+  mm_retain   IBOutlet NSView   *_groupColorView;
+  mm_retain   IBOutlet NSView   *_groupFontView;
+  mm_unretain IBOutlet NSPopUpButton *_settingsChooser;
+  mm_unretain IBOutlet NSColorWell *_wellDarkBackground;
+  mm_unretain IBOutlet NSColorWell *_wellDarkInsertionPoint;
+  mm_unretain IBOutlet NSColorWell *_wellDarkOperand;
+  mm_unretain IBOutlet NSColorWell *_wellDarkOperator;
+  mm_unretain IBOutlet NSColorWell *_wellDarkOther;
+  mm_unretain IBOutlet NSColorWell *_wellDarkParenthesis;
+  mm_unretain IBOutlet NSColorWell *_wellDarkSolution;
+  mm_unretain IBOutlet NSColorWell *_wellLightBackground;
+  mm_unretain IBOutlet NSColorWell *_wellLightError;
+  mm_unretain IBOutlet NSColorWell *_wellLightInsertionPoint;
+  mm_unretain IBOutlet NSColorWell *_wellLightOperand;
+  mm_unretain IBOutlet NSColorWell *_wellLightOperator;
+  mm_unretain IBOutlet NSColorWell *_wellLightOther;
+  mm_unretain IBOutlet NSColorWell *_wellLightParenthesis;
+  mm_unretain IBOutlet NSColorWell *_wellLightSolution;
 }
 
 -(void)awakeFromNib;
--(IBAction)choiceChanged:(NSControl*)sender;
--(IBAction)valueChanged:(NSControl*)sender;
--(IBAction)valueReset:(NSControl*)sender;
 
+-(IBAction)choiceChanged:(NSPopUpButton*)sender;
+-(IBAction)fontChangeRequest:(NSButton*)sender;
+-(IBAction)colorChanged:(NSColorWell*)sender;
+-(IBAction)timeChanged:(NSTextField*)sender;
+-(IBAction)themeChanged:(NSPopUpButton*)sender;
+
+-(IBAction)fontReset:(NSButton*)sender;
+-(IBAction)colorReset:(NSButton*)sender;
+-(IBAction)timeReset:(NSButton*)sender;
+
+-(BOOL)themeColor:(SVRThemeColor*)colorPointer
+   interfaceStyle:(XPUserInterfaceStyle*)stylePointer
+     forColorWell:(NSColorWell*)sender;
+
+-(BOOL)themeFont:(SVRThemeFont*)fontPointer
+       forButton:(NSButton*)sender;
 
 
 @end
--- Soulver/SVRSettingsViewController.m
+++ Soulver/SVRSettingsViewController.m
@@ -19,6 +19,8 @@
   
   [XPLog debug:@"choiceChanged:%@(%ld)", sender, selection];
   
+  return;
+  
   [_groupFontView removeFromSuperview];
   [_groupColorView removeFromSuperview];
   [_groupGeneralView removeFromSuperview];
@@ -38,14 +40,116 @@
   }
 }
 
--(void)valueChanged:(NSControl*)sender;
+-(IBAction)fontChangeRequest:(NSButton*)sender;
 {
-  NSLog(@"valueChanged:%@", sender);
+  NSLog(@"fontChangeRequest:%@", sender);
 }
 
--(void)valueReset:(NSControl*)sender;
+-(IBAction)colorChanged:(NSColorWell*)sender;
 {
-  NSLog(@"valueReset:%@", sender);
+  NSLog(@"colorChanged:%@", sender);
+}
+
+-(IBAction)timeChanged:(NSTextField*)sender;
+{
+  NSLog(@"timeChanged:%@", sender);
+}
+
+-(IBAction)themeChanged:(NSPopUpButton*)sender;
+{
+  NSLog(@"themeChanged:%@", sender);
+}
+
+-(IBAction)fontReset:(NSButton*)sender;
+{
+  NSLog(@"fontReset:%@", sender);
+}
+
+-(IBAction)colorReset:(NSButton*)sender;
+{
+  NSLog(@"colorReset:%@", sender);
+}
+
+-(IBAction)timeReset:(NSButton*)sender;
+{
+  NSLog(@"timeReset:%@", sender);
+}
+
+-(BOOL)themeColor:(SVRThemeColor*)colorPointer
+   interfaceStyle:(XPUserInterfaceStyle*)stylePointer
+     forColorWell:(NSColorWell*)sender;
+{
+  *colorPointer = -2;
+  *stylePointer = -2;
+  
+  if        (sender == _wellDarkBackground) {
+    *colorPointer = SVRThemeColorBackground;
+    *stylePointer = XPUserInterfaceStyleDark;
+    return YES;
+  } else if (sender == _wellDarkInsertionPoint) {
+    *colorPointer = SVRThemeColorInsertionPoint;
+    *stylePointer = XPUserInterfaceStyleDark;
+    return YES;
+  } else if (sender == _wellDarkOperand) {
+    *colorPointer = SVRThemeColorOperand;
+    *stylePointer = XPUserInterfaceStyleDark;
+    return YES;
+  } else if (sender == _wellDarkOperator) {
+    *colorPointer = SVRThemeColorOperator;
+    *stylePointer = XPUserInterfaceStyleDark;
+    return YES;
+  } else if (sender == _wellDarkOther) {
+    *colorPointer = SVRThemeColorOtherText;
+    *stylePointer = XPUserInterfaceStyleDark;
+    return YES;
+  } else if (sender == _wellDarkParenthesis) {
+    *colorPointer = SVRThemeColorBracket;
+    *stylePointer = XPUserInterfaceStyleDark;
+    return YES;
+  } else if (sender == _wellDarkSolution) {
+    *colorPointer = SVRThemeColorSolution;
+    *stylePointer = XPUserInterfaceStyleDark;
+    return YES;
+  } else if (sender == _wellLightBackground) {
+    *colorPointer = SVRThemeColorBackground;
+    *stylePointer = XPUserInterfaceStyleLight;
+    return YES;
+  } else if (sender == _wellLightError) {
+    *colorPointer = SVRThemeColorErrorText;
+    *stylePointer = XPUserInterfaceStyleLight;
+    return YES;
+  } else if (sender == _wellLightInsertionPoint) {
+    *colorPointer = SVRThemeColorInsertionPoint;
+    *stylePointer = XPUserInterfaceStyleLight;
+    return YES;
+  } else if (sender == _wellLightOperand) {
+    *colorPointer = SVRThemeColorOperand;
+    *stylePointer = XPUserInterfaceStyleLight;
+    return YES;
+  } else if (sender == _wellLightOperator) {
+    *colorPointer = SVRThemeColorOperator;
+    *stylePointer = XPUserInterfaceStyleLight;
+    return YES;
+  } else if (sender == _wellLightOther) {
+    *colorPointer = SVRThemeColorOtherText;
+    *stylePointer = XPUserInterfaceStyleLight;
+    return YES;
+  } else if (sender == _wellLightParenthesis) {
+    *colorPointer = SVRThemeColorBracket;
+    *stylePointer = XPUserInterfaceStyleLight;
+    return YES;
+  } else if (sender == _wellLightSolution) {
+    *colorPointer = SVRThemeColorSolution;
+    *stylePointer = XPUserInterfaceStyleLight;
+    return YES;
+  }
+  return NO;
+}
+
+-(BOOL)themeFont:(SVRThemeFont*)fontPointer
+       forButton:(NSButton*)sender;
+{
+  return NO;
 }
 
 @end
